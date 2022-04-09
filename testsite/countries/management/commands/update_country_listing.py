48c9d5df4e09a63536d8fa8434b596022decfa4f
muhammadraja <43963104+muhammadraja@users.noreply.github.com>
2022-04-09 14:56:24 +0100

    update api response, add fields, update command

--- testsite/countries/management/commands/update_country_listing.py
+++ testsite/countries/management/commands/update_country_listing.py
@@ -1,5 +1,6 @@
 import json
 import os
+import requests
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -10,11 +11,16 @@ from countries.models import Country, Region
 class Command(BaseCommand):
     help = "Loads country data from a JSON file."
 
-    IMPORT_FILE = os.path.join(settings.BASE_DIR, "..", "data", "countries.json")
+    # IMPORT_FILE = os.path.join(settings.BASE_DIR, "..", "data", "countries.json")
 
     def get_data(self):
-        with open(self.IMPORT_FILE) as f:
-            data = json.load(f)
+        # with open(self.IMPORT_FILE) as f:
+        #     data = json.load(f)
+
+        # pull data from the api
+        url = "https://storage.googleapis.com/dcr-django-test/countries.json"
+        raw_response = requests.get(url)
+        data = raw_response.json()
         return data
 
     def handle(self, *args, **options):
@@ -31,6 +37,8 @@ class Command(BaseCommand):
                     "alpha2Code": row["alpha2Code"],
                     "alpha3Code": row["alpha3Code"],
                     "population": row["population"],
+                    "topLevelDomain": row["topLevelDomain"],
+                    "capital": row["capital"],
                     "region": region,
                 },
             )
--- testsite/countries/views.py
+++ testsite/countries/views.py
@@ -1,8 +1,32 @@
 from django.http import JsonResponse
-
+from django.db.models import Sum
+from .models import Country, Region
 
 def stats(request):
     # TODO - Provide name, number_countries and total_population for each region
-    response = {"regions": []}
+
+    total_population = 0 # default value
+    data = [] # build the data list to return
+
+    regions = Region.objects.all() # get all regions
+
+    for region in regions:
+        print('\nregion: ', region)
+        for country in Country.objects.filter(region=region):
+            print('country: ', country)
+            print('country.population: ', country.population)
+            number_countries = Country.objects.filter(region=region).count()
+            # total_population = Country.objects.filter(region=region).aggregate(Sum('population'))
+            total_population += country.population
+            print('number_countries: ', number_countries)
+            print('total_population: ', total_population)
+            
+        data.append([{"name": region.name, 
+            "number_countries": number_countries,
+            "total_population": total_population
+        }])
+        total_population = 0 # reset total_population
+
+    response = {"regions": data}
 
     return JsonResponse(response)
